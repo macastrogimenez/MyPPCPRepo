e908a74186758a849506162150d73cff422dd720
macastrogimenez <migca@itu.dk>
2025-09-06 17:38:20 +0200

    remove outdated classes and add ReadWriteMonitor and ReadersWriters implementations for  exercise02

---
+++ Assigment1/exercise02/week02exercises/app/src/main/java/exercises02/ReadWriteMonitor.java
@@ -0,0 +1,72 @@
+package exercises02;
+
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+import java.util.concurrent.locks.Condition;
+
+public class ReadWriteMonitor {
+    private int readers         = 0;
+    private boolean writer      = false;
+    private Lock lock           = new ReentrantLock();
+    private Condition condition = lock.newCondition();
+
+
+    //////////////////////////
+    // Read lock operations //
+    //////////////////////////
+
+    public synchronized void readLock() {
+        try {
+            while(writer)
+                condition.await();
+            readers++;
+        }
+        catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void readUnlock() {
+        lock.lock();
+        try {
+            readers--;
+            if(readers==0)
+                condition.signalAll();
+        }
+        finally {
+            lock.unlock();
+        }
+    }
+
+
+    ///////////////////////////
+    // Write lock operations //
+    ///////////////////////////
+
+    public void writeLock() {
+        lock.lock();
+        try {
+            while(readers > 0 || writer)
+                condition.await();
+            writer=true;
+        }
+        catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        finally {
+            lock.unlock();
+        }
+    }
+
+    public void writeUnlock() {
+        lock.lock();
+        try {
+            writer=false;
+            condition.signalAll();
+        }
+        finally {
+            lock.unlock();
+        }
+    }
+
+}
