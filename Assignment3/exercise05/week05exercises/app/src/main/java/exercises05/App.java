0985a1f2c7f83f058106517645111d121c1ca884
macastrogimenez <migca@itu.dk>
2025-09-28 21:43:56 +0200

    Exercise5 - assignment 3: initialize Gradle project structure with basic application setup and semaphore implementation

---
+++ Assignment3/exercise05/week05exercises/app/src/main/java/exercises05/App.java
@@ -0,0 +1,9 @@
+package exercises05;
+
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello!");
+    }
+}
+
+// line to run the file on the terminal: gradle "-PmainClass=exercises05.App" run
